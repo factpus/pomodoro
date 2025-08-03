bb66fc170e56a5341063463596a73d0fd8dfd41a
keita mori <mk5.noki@gmail.com>
2025-08-03 20:20:22 +0900

    mod favicon and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
       </div>
 
       <footer className="text-center mt-8 text-gray-500">
-          <p>Made with ❤️ by you & Gemini</p>
+          <p>Made with</p>
       </footer>
     </main>
   );
